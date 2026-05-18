52534313b9440c5b4c89da8bc738547a012cdf57
Adam <adam@Adams-Mac-mini.local>
2026-05-19 01:25:19 +0200

    fix(vintage-drifter): change mask from linear-gradient to solid radial-gradient to fix gigantic mobile viewport scale explosion bug in Safari WebKit

--- VintageDrifter_UI.jsx
+++ VintageDrifter_UI.jsx
@@ -8024,8 +8024,8 @@ export default function App({ websiteMode = false, showCables = true }) {
               transform: `scale(${OUTER_PLUGIN_SCALE}) translateZ(0)`, 
               transformOrigin: 'center center', 
               borderRadius: FRAMES[frameStyle]?.panelRadius || '4rem',
-              WebkitMaskImage: '-webkit-linear-gradient(white, white)',
-              maskImage: 'linear-gradient(white, white)',
+              WebkitMaskImage: '-webkit-radial-gradient(white, white)',
+              maskImage: 'radial-gradient(white, white)',
               isolation: 'isolate',
               ...FRAMES[frameStyle]?.panelStyle 
             }}
